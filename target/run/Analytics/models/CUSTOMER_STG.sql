09fb50674c402eeddcc1e12eee1dd0ef2040cf2b
Busi Tarun Kumar <busi.tarun.kumar@pwc.com>
2025-05-18 02:59:36 +0530

    Added files from master to main

---
+++ target/run/Analytics/models/CUSTOMER_STG.sql
@@ -0,0 +1,20 @@
+
+  create or replace   view EMART_OMS.L3_CONSUMPTION.CUSTOMER_STG
+  
+   as (
+    SELECT
+    CUSTOMERID,
+    FIRSTNAME,
+    LASTNAME,
+    EMAIL,
+    PHONE,
+    ADDRESS,
+    CITY,
+    STATE,
+    ZIPCODE,
+    UPDATED_AT,
+    CONCAT(FIRSTNAME, ' ', LASTNAME) AS CUSTOMERNAME
+FROM
+    EMART_OMS.L1_LANDING.CUSTOMERS
+  );
+
